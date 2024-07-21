03546563f50d43a0cc8db4af17c5e1c0baace3b7
Jeff Nyman <jeffnyman@gmail.com>
2024-07-21 08:04:55 -0500

    Modify structure of matchers for import.

--- src/expectify/__init__.py
+++ src/expectify/__init__.py
@@ -1,3 +1,4 @@
 from .expect import expect
+from .matchers import built_in
 
-__all__ = ["expect"]
+__all__ = ["expect"] + built_in.__all__
---
+++ src/expectify/matchers/built_in/__init__.py
@@ -0,0 +1,5 @@
+from .equal import equal
+
+__all__ = [
+    "equal"
+]
