2a9b660cb34792bfa60f19c3dd691f8b3a95dee7
Jeff Nyman <jeffnyman@gmail.com>
2024-07-21 09:30:58 -0500

    Add failure message to matcher.

--- src/expectify/matchers/__init__.py
+++ src/expectify/matchers/__init__.py
@@ -1,3 +1,12 @@
 class Matcher:
     def _match(self, subject):
         raise NotImplementedError()
+
+    def _failure_message(self, subject, reasons):
+        message = "\nexpected: {subject!r} to {matcher!r}".format(
+            subject=subject, matcher=self)
+
+        if reasons:
+            message += "\n     but: {0}".format("\n          ".join(reasons))
+
+        return message
