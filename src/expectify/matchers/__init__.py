859eaf61d9d4ecc5bfd414f446897689e9649374
Jeff Nyman <jeffnyman@gmail.com>
2024-07-21 09:41:49 -0500

    Add string representation for matcher.

--- src/expectify/matchers/__init__.py
+++ src/expectify/matchers/__init__.py
@@ -10,3 +10,13 @@ class Matcher:
             message += "\n     but: {0}".format("\n          ".join(reasons))
 
         return message
+
+    def __repr__(self):
+        if hasattr(self, "_expected"):
+            return "{name} {expected!r}".format(name=self._name, expected=self._expected)
+
+        return self._name
+
+    @property
+    def _name(self):
+        return type(self).__name__.replace("_", " ").strip()
