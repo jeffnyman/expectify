4f040e708d348b4874e982ad7439e9e56f0d0e88
Jeff Nyman <jeffnyman@gmail.com>
2024-07-21 10:25:14 -0500

    Refactor matcher string display.

--- src/expectify/matchers/__init__.py
+++ src/expectify/matchers/__init__.py
@@ -3,17 +3,16 @@ class Matcher:
         raise NotImplementedError()
 
     def _failure_message(self, subject, reasons):
-        message = "\nexpected: {subject!r} to {matcher!r}".format(
-            subject=subject, matcher=self)
+        message = f"\nexpected: {subject!r} to {self!r}"
 
         if reasons:
-            message += "\n     but: {0}".format("\n          ".join(reasons))
+            message += f"\n     but: {'\n          '.join(reasons)}"
 
         return message
 
     def __repr__(self):
         if hasattr(self, "_expected"):
-            return "{name} {expected!r}".format(name=self._name, expected=self._expected)
+            return f"{self._name} {self._expected!r}"
 
         return self._name
 
