f1a3134967db6eb222665140275e8b3e023e4f48
Jeff Nyman <jeffnyman@gmail.com>
2024-07-21 10:15:28 -0500

    Add traceback hack for pytest.

--- src/expectify/expectation.py
+++ src/expectify/expectation.py
@@ -3,9 +3,13 @@ class Expectation:
         self._subject = subject
 
     def to(self, matcher):
+        __tracebackhide__ = True
+
         self._assert(matcher)
 
     def _assert(self, matcher):
+        __tracebackhide__ = True
+
         passed, reasons = self._match(matcher)
 
         if not passed:
