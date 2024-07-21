5e4f77263a0569cef980402c223259e348827abe
Jeff Nyman <jeffnyman@gmail.com>
2024-07-21 07:36:10 -0500

    Add core expectation logic.

--- src/expectify/expectation.py
+++ src/expectify/expectation.py
@@ -1,3 +1,18 @@
 class Expectation:
     def __init__(self, subject):
         self._subject = subject
+
+    def to(self, matcher):
+        self._assert(matcher)
+
+    def _assert(self, matcher):
+        passed, reasons = self._match(matcher)
+
+        if not passed:
+            raise AssertionError(self._failure_reason(matcher, reasons))
+
+    def _match(self, matcher):
+        return getattr(matcher, "_match")(self._subject)
+
+    def _failure_reason(self, matcher, *args):
+        return getattr(matcher, "_failure_message")(self._subject, *args)
