b2f2664d8e76329a6be0b9f39e32d78eb35bac9a
Jeff Nyman <jeffnyman@gmail.com>
2024-07-21 08:20:40 -0500

    Add equal matcher.

--- src/expectify/matchers/built_in/equal.py
+++ src/expectify/matchers/built_in/equal.py
@@ -4,3 +4,6 @@ from .. import Matcher
 class equal(Matcher):
     def __init__(self, expected):
         self._expected = expected
+
+    def _match(self, subject):
+        return subject == self._expected, []
