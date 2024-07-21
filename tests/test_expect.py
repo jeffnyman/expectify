780ee2e60e1bda75f0e9a28dda83ad2e8880ae58
Jeff Nyman <jeffnyman@gmail.com>
2024-07-21 09:59:46 -0500

    Add testing framework.

---
+++ tests/test_expect.py
@@ -0,0 +1,13 @@
+import pytest
+
+from expectify import expect
+from expectify.matchers.built_in.equal import equal
+
+
+def test_expect_equal_pass():
+    expect("jeff").to(equal("jeff"))
+
+
+def test_expect_equal_fail():
+    with pytest.raises(AssertionError):
+        expect("jeff").to(equal("geoff"))
