8d521a4dee0d98704c1b1aa2f75c1da5905c6f21
Jeff Nyman <jeffnyman@gmail.com>
2024-12-18 09:36:58 -0600

    Stub logic for asserting on a matcher.

--- src/expectify/expectation.py
+++ src/expectify/expectation.py
@@ -8,6 +8,13 @@ class Expectation:
     def __init__(self, subject) -> None:
         self._subject = subject
 
-    @staticmethod
-    def to(matcher: "Matcher") -> None:
+    def to(self, matcher: "Matcher") -> None:
         print(f"... check for matcher: {matcher}.")
+
+        self._assert(matcher)
+
+    def _assert(self, matcher):
+        self._match(matcher)
+
+    def _match(self, matcher):
+        print(f"Attempting assert based on {matcher}")
