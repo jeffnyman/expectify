abb9a31de06a5871b1d6c0f9be4fcee914aae823
Jeff Nyman <jeffnyman@gmail.com>
2024-12-19 03:17:43 -0600

    Provide expectation failure message.

--- src/expectify/matchers/__init__.py
+++ src/expectify/matchers/__init__.py
@@ -1,8 +1,18 @@
 class Matcher:
     def _failure_message(self, subject: str, reasons: list) -> str:
-        return f"Expected: {subject}; {self}; {reasons}"
+        message = "\nexpected: {subject} to {matcher}".format(
+            subject=subject, matcher=self
+        )
+
+        if reasons:
+            message += "\n     but: {0}".format("\n          ".join(reasons))
+
+        return message
 
     def __repr__(self) -> str:
+        if hasattr(self, "_expected"):
+            return "{name} {expected}".format(name=self._name, expected=self._expected)
+
         return self._name
 
     @property
