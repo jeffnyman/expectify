74df6c3f98bccd650c45857d4730bfbb06105324
Jeff Nyman <jeffnyman@gmail.com>
2024-12-19 03:35:32 -0600

    Add tests.

--- tests/test_expect.py
+++ tests/test_expect.py
@@ -1,5 +1,12 @@
+import pytest
+
 from expectify import expect, equal
 
 
-def test_expect() -> None:
+def test_expect_equal_pass() -> None:
     expect("jeff").to(equal("jeff"))
+
+
+def test_expect_equal_fail() -> None:
+    with pytest.raises(AssertionError):
+        expect("jeff").to(equal("geoff"))
