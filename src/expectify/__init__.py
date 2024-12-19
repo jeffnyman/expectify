81da80e86a72e82337ccf738732bd64ef565458e
Jeff Nyman <jeffnyman@gmail.com>
2024-12-19 04:54:26 -0600

    Refactor of matcher inclusion.

--- src/expectify/__init__.py
+++ src/expectify/__init__.py
@@ -2,4 +2,4 @@ from .expect import expect
 from .matchers import standard
 from .matchers.standard import *  # noqa: F403
 
-__all__ = ["expect"] + standard.__all__
+__all__ = ["expect", *standard.__all__]
