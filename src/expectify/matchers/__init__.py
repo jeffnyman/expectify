0d1f2dab8fd0f15fbaee09fdcce8171c48c71253
Jeff Nyman <jeffnyman@gmail.com>
2024-12-19 01:32:12 -0600

    Add matcher representation.

--- src/expectify/matchers/__init__.py
+++ src/expectify/matchers/__init__.py
@@ -1,3 +1,10 @@
 class Matcher:
     def _failure_message(self, subject: str, reasons: list) -> str:
         return f"Expected: {subject}; {self}; {reasons}"
+
+    def __repr__(self) -> str:
+        return self._name
+
+    @property
+    def _name(self) -> str:
+        return type(self).__name__
